eaeb1629ff638f345d816ea2321b2ef7ee3163f6
a.mulyar <frunicefrun@gmail.com>
2025-11-05 16:56:36 +0300

    npm netric 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/portfolioPrototype",
-    output: "export",
-    reactStrictMode: true,
-  };
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://plausible.io;
+              connect-src 'self' https://plausible.io;
+              img-src 'self' blob: data:;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
+              frame-ancestors 'none';
+              base-uri 'self';
+              form-action 'self';
+            `.replace(/\s{2,}/g, ' ').trim()
+          }
+        ],
+      },
+    ];
+  },
+  basePath: "/portfolioPrototype",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
