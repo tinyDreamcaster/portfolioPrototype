458ea994ccb88c7c02af4355678b558b7f527961
a.mulyar <frunicefrun@gmail.com>
2025-11-05 18:16:29 +0300

    npm metric tracker

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import "./globals.scss";
 import PlausibleProvider from "next-plausible";
-
+import Script from "next/script";
 
 
 export const metadata = {
@@ -9,13 +9,35 @@ export const metadata = {
   description: "Прототип портфолио веб-дизайнера",
 };
 
-init({
-  domain: 'tinydreamcaster.github.io/portfolioPrototype'
-})
+
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          src="https://plausible.io/js/pa-o4ooHjXTAqrjqudPiw7tX.js"
+        />
+        <Script
+          id="plausible-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.plausible = window.plausible || function() { 
+                (window.plausible.q = window.plausible.q || []).push(arguments) 
+              };
+              window.plausible.init = window.plausible.init || function(i) { 
+                window.plausible.o = i || {}; 
+              };
+              window.plausible.init();
+            `,
+          }}
+        />
+
+      </head>
       <PlausibleProvider domain="tinydreamcaster.github.io/portfolioPrototype">
         <body className={'pageBody'}>{children}</body>
       </PlausibleProvider>
